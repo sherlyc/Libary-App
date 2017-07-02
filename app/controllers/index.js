b99b9f78b73e8d30f92c3acb017c18b8f0dd4397
Sherly Chan <sherlyc@gmail.com>
2017-07-02 21:49:07 +1200

    added promise to return id from firebase

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -13,9 +13,10 @@ export default Ember.Controller.extend({
       saveInvitation() {
           const email = this.get('emailAddress');
           const newInvitation = this.store.createRecord('invitation', { email: email });
-          newInvitation.save();
-          this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-          this.set('emailAddress', '');
-      }
-  }
+          newInvitation.save().then((response) => {
+            this.set('responseMessage', `Thank you! We've just saved your email address with the following id: ${response.get('id')}`);
+            this.set('emailAddress', '');
+          });
+     }
+ }
 });
